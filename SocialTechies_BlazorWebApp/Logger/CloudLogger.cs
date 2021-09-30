3edfe6eaa1954e67620edf58df72059b9fca2db1
Dhana <dhana.manimaran@emplifi.io>
2021-09-30 00:56:12 -0400

    Added - Background task to trigger the slake and DateTime - Logger and configuration TODO: Add slake URL

---
+++ SocialTechies_BlazorWebApp/Logger/CloudLogger.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SocialTechies_BlazorWebApp.Logger
+{
+    public static class CloudLogger
+    {
+        public static void logSlack(string message, string slackUrl)
+        {
+            try
+            {
+                if (!String.IsNullOrWhiteSpace(slackUrl))
+                {
+                    var content = JsonConvert.SerializeObject(new
+                    {
+                        text = message
+                    });
+                    DoWebRequest("POST", slackUrl, content, "application/json", "application/json, text/javascript, */*; q=0.01", out String response, out String error);
+                }
+            }
+            catch { /* eat */ }
+        }
+
+        // dont want to use utilities here...
+        public static bool DoWebRequest(String verb, String url, String postData, String contentType, String dataType, out String strResponse, out String error)
+        {
+            strResponse = "";
+            error = "";
+
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3;
+
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(url);
+            req.UserAgent = "AstuteSocial";
+            req.ServicePoint.Expect100Continue = false;
+            req.Method = verb;
+            req.UseDefaultCredentials = true;
+
+            if (dataType != null) req.Accept = dataType;
+            if (contentType != null) req.ContentType = contentType;
+
+            using (StreamWriter sw = new StreamWriter(req.GetRequestStream()))
+            {
+                sw.Write(postData);
+                sw.Close();
+            }
+
+            try
+            {
+                using (HttpWebResponse resp = (HttpWebResponse)req.GetResponse())
+                {
+                    if (resp != null)
+                    {
+                        using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+                        {
+                            strResponse = sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                error = ex.Message;
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- SocialTechies_BlazorWebApp/Workers/TimesBackgroundTask.cs
+++ SocialTechies_BlazorWebApp/Workers/TimesBackgroundTask.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SocialTechies_BlazorWebApp.Data.Aws;
+using SocialTechies_BlazorWebApp.Logger;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +16,20 @@ namespace SocialTechies_BlazorWebApp.Workers
         private int executionCount = 0;
         private readonly ILogger<TimesBackgroundTask> _logger;
         private Timer _timer;
+        int period = 3600;
+        DateTime startTime = DateTime.Now.AddMinutes(30);
+        DateTime endTime = DateTime.Now;
+        AwsService aws;
+        private readonly IConfiguration _Configuration;
         public TimesBackgroundTask(
-            ILogger<TimesBackgroundTask> logger)
+            ILogger<TimesBackgroundTask> logger,
+            IConfiguration configuration)
         {
             try
             {
+                aws = new AwsService();
                 _logger = logger;
+                _Configuration = configuration;
             }
             catch (Exception)
             { }
@@ -26,18 +37,24 @@ namespace SocialTechies_BlazorWebApp.Workers
 
         public virtual Task StartAsync(CancellationToken stoppingToken)
         {
+
             _logger.LogInformation("Timed Hosted Service running.");
             _timer = new Timer(DoWork, null, TimeSpan.Zero,
-                TimeSpan.FromSeconds(30));
+                TimeSpan.FromSeconds(10));
             return Task.CompletedTask;
         }
 
         private void DoWork(object state)
         {
             var count = Interlocked.Increment(ref executionCount);
-
-            _logger.LogInformation(
-                "Timed Hosted Service is working. Count: {Count}", count);
+            var autoscalingGroup = _Configuration["autoscalingGroup"];
+            Task<EcsMetrics.CpuUtilization> cpuUtilization = aws.GetCpuUtilizationForAutoscalingGroup(autoscalingGroup, period, startTime, endTime);
+            if (cpuUtilization?.Result?.Datapoints?.OrderByDescending(t => t.TimeStamp).First().Maximum > 5)
+            {
+                //Call Slake 
+                //TODO: Add Slake URL
+                CloudLogger.logSlack($"{autoscalingGroup} group utilized > 5. Value is {cpuUtilization.Result.Datapoints.OrderByDescending(t => t.TimeStamp).First().Maximum}","");
+            }
         }
 
         public virtual Task StopAsync(CancellationToken stoppingToken)
